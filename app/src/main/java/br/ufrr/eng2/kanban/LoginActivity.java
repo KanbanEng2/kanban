ab2199264710dcc3fdd132b4e59fc7d8085098a9
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2016-11-24 11:27:11 -0400

    Added LoginActivity

--- app/src/main/java/br/ufrr/eng2/kanban/LoginActivity.java
+++ app/src/main/java/br/ufrr/eng2/kanban/LoginActivity.java
@@ -1,13 +1,24 @@
 package br.ufrr.eng2.kanban;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class LoginActivity extends AppCompatActivity {
+
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
+
+    public void gotoMain(View v) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
 }
