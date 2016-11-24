095c8736a3f9f7c5dfd61a80a67ad0c9e2ba1e1b
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2016-11-24 09:07:10 -0400

    Added LoginActivity

---
+++ app/src/main/java/br/ufrr/eng2/kanban/LoginActivity.java
@@ -0,0 +1,13 @@
+package br.ufrr.eng2.kanban;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
