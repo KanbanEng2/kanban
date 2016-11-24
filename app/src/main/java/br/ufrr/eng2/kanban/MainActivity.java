5f738f37091c0d85960987d4cf54421c6fa2c7e9
Rodrigo Tavares <rodrigost23@gmail.com>
2016-11-23 21:17:05 -0400

    New Task activity with proper animation

--- app/src/main/java/br/ufrr/eng2/kanban/MainActivity.java
+++ app/src/main/java/br/ufrr/eng2/kanban/MainActivity.java
@@ -1,11 +1,19 @@
 package br.ufrr.eng2.kanban;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.RectF;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.SharedElementCallback;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.transition.Transition;
+import android.util.Pair;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -16,6 +24,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import java.util.List;
+import java.util.Map;
 
 import br.ufrr.eng2.kanban.adapter.CardsAdapter;
 
@@ -56,10 +67,20 @@ public class MainActivity extends AppCompatActivity
         mRecyclerView.setLayoutManager(mLayoutManager);
         mAdapter = new CardsAdapter();
         mRecyclerView.setAdapter(mAdapter);
+
+        getWindow().getSharedElementExitTransition().excludeTarget(R.id.appBarLayout, true);
+        getWindow().getSharedElementExitTransition().excludeChildren(R.id.appBarLayout, true);
+
         mAdapter.setOnItemClickListener(new CardsAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(View view, long position) {
-                startActivity(new Intent(MainActivity.this, TaskActivity.class));
+                ActivityOptionsCompat options = ActivityOptionsCompat.
+                        makeSceneTransitionAnimation(MainActivity.this,
+                                android.support.v4.util.Pair.create(view.findViewById(R.id.card_view), "task_background"),
+                                android.support.v4.util.Pair.create(view.findViewById(R.id.card_tag_color), "task_color")
+                        );
+
+                startActivity(new Intent(MainActivity.this, TaskActivity.class), options.toBundle());
             }
         });
     }
--- app/src/main/java/br/ufrr/eng2/kanban/TaskActivity.java
+++ app/src/main/java/br/ufrr/eng2/kanban/TaskActivity.java
@@ -1,16 +1,82 @@
 package br.ufrr.eng2.kanban;
 
+import android.animation.Animator;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.os.Build;
+import android.os.Parcelable;
+import android.support.v4.app.SharedElementCallback;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.transition.Transition;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.animation.AnimationSet;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
-public class TaskActivity extends AppCompatActivity {
+import java.util.List;
+
+public class TaskActivity extends AppCompatActivity implements Transition.TransitionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        }
+
         setContentView(R.layout.activity_task);
         Toolbar toolbar = (Toolbar) findViewById(R.id.task_toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getWindow().getSharedElementEnterTransition().addListener(this);
+    }
+
+    @Override
+    public void onTransitionStart(Transition transition) {
+        transition.excludeTarget(R.id.appBarLayout, true);
+    }
+
+    @Override
+    public void onTransitionEnd(Transition transition) {
+        transition.removeListener(this);
+        View toolbarBg = findViewById(R.id.toolbar_background);
+        View toolbar = findViewById(R.id.task_toolbar);
+
+        int cx = (toolbarBg.getLeft() + toolbarBg.getRight()) / 2;
+        int cy = (toolbarBg.getTop() + toolbarBg.getBottom()) / 2;
+
+        Animator circularReveal =
+                ViewAnimationUtils.createCircularReveal(toolbarBg, cx, cy, 0, toolbar.getWidth());
+
+        circularReveal.start();
+
+        RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) toolbarBg.getLayoutParams();
+        params.addRule(RelativeLayout.ALIGN_PARENT_END);
+        toolbarBg.setLayoutParams(params);
+    }
+
+    @Override
+    public void onTransitionCancel(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionPause(Transition transition) {
+
+    }
+
+    @Override
+    public void onTransitionResume(Transition transition) {
+
     }
 }
